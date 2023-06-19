2f9240e809df475d7616b2207fd5bf5f7fdbd6b3
Clayton C. Silva <claytoncaitanosilva1995@gmail.com>
2023-06-19 14:59:51 -0300

    Repository and Service layers created for 'Categoria' in DB

---
+++ src/main/java/com/ccsilva/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.ccsilva.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ccsilva.cursomc.domain.Categoria;
+import com.ccsilva.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
